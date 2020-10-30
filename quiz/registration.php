ceedb20dfd9a4c10274488190ac47070276ea7c5
RIYA SONI <soniriya75710@gmail.com>
2020-10-30 16:38:32 +0530

    On branch online_test_platform

--- quiz/registration.php
+++ quiz/registration.php
@@ -1,5 +1,5 @@
 <?php
-include('config.php');
+include('../admin/config.php');
 $errors = array();
 $message = '';
 if (isset($_POST['submit'])) {
@@ -32,6 +32,8 @@ if (isset($_POST['submit'])) {
 
         if ($conn->query($sql) === true) {
             //echo "New record created successfully";
+            header('Location:login.php');
+                exit;
         } else {
             $errors[] = array('query' => 'form', 'msg' => $conn->error);
             //echo "Error: " . $sql . "<br>" . $conn->error;
@@ -41,21 +43,7 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>Registration</title>
-</head>
-
-<body>
+<?php include('header.php'); ?>
     <div class="container">
         <h1 class="text-center text-success">Online Quiz</h1>
         <div class="card">
@@ -137,11 +125,4 @@ if (isset($_POST['submit'])) {
         </div>
     </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
-</html>
+    <?php include('footer.php'); ?>
---
+++ quiz/test.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+include('../admin/config.php');
+if (!isset($_SESSION['studentdata']['name'])) {
+    header('Location:login.php');
+}
+
+$name = $_SESSION['studentdata']['name'];
+?>
+<?php include('header.php'); ?>
+    <div class="container">
+        <br>
+        <h1 class="text-center text-primary">Online Quiz</h1><br>
+        <div class="col-lg-8 m-auto d-block">
+            <div class="card">
+                <h2 class="text-center text-success card-header">Welcome <?php echo $name; ?>,Best Of Luck</h2><br>
+            </div><br>
+            <h2 class="text-center text-primary">select any quiz</h2><br>
+    <div class="col-lg-8 m-auto d-block">
+        <a href="quiz1.php" class="btn btn-success">Quiz1</a>
+        <a href="quiz2.php" class="btn btn-success">Quiz2</a>
+    </div><br><br>
+    <div class="m-auto d-block">
+        <a href="login.php?name=logout" class="btn btn-primary">logout</a>
+    </div>
+    </div>
+
+
+
+
+
+    <?php include('footer.php'); ?>
